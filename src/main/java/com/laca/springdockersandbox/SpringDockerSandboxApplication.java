7b60978bd9880b6d4cab5c709d42c224a3f99fb7
laszlogulyas.jr@gmail.com <laszlogulyas.jr@gmail.com>
2021-08-03 17:13:07 +0200

    init

---
+++ src/main/java/com/laca/springdockersandbox/SpringDockerSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.laca.springdockersandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDockerSandboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/laca/springdockersandbox/SpringDockerSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laca.springdockersandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerSandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
